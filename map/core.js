79f7711cdd4ee71b0f3b4f7f3763e2f7218c485c
KatieAJenkins <kind2karma@gmail.com>
2017-03-02 06:59:20 -0700

    test 1 passing multiple by 10

--- map/core.js
+++ map/core.js
@@ -1,6 +1,18 @@
+'use strict';
+
+var mapArray = [ 45, 1, -10, 11, 250 ];
+
 function multiplyBy10 (array) {
  // your code here
-};
+  return array.map(function(number){
+   console.log(number);
+   console.log(number * 10);
+   console.log(mapArray);
+   return number * 10;
+ });
+}
+
+multiplyBy10(mapArray);
 
 function shiftRight (array) {
  // your code here
