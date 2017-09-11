4d1ac23d8a4087029dfa48c5e6887ffb41a2e011
KatieAJenkins <kind2karma@gmail.com>
2017-09-11 11:50:48 -0600

    working on reduce

--- reduce/core.js
+++ reduce/core.js
@@ -5,6 +5,8 @@
 //   return sum;
 // }
 
+//reduce = combine all elements in an array into one!
+
 //reduce starts by giving you access to the first two values in the array (previous, current)
 
 //then return a SINGLE value that will be used as PREVIOUS the next time around
@@ -29,33 +31,94 @@ var array = [ 10, 15, 20, 25, 30, 35 ]
 
 function sum (array) {
   // your code here
-   let sum = array.reduce(function(previous, current, index){
-    console.log('previous', previous);
+   let sum = array.reduce(function(accumulator, current, index, array){
+    console.log('accumulator', accumulator);
     console.log('current', current);
     console.log('index', index);
-    return previous + current; //this returns a single number used for previous in the next interation
-  },0)
-  console.log('sum', sum);
+    console.log('array', array);
+   //this returns a single number used for previous in the next interation
+     return previous + current;
+  })
   return sum;
 };
 
+// function sum (array) {
+//   // your code here
+//    let sum = array.reduce(function(previous, current, index){
+//     // console.log('previous', previous);
+//     // console.log('current', current);
+//     // console.log('index', index);
+//     return previous + current; //this returns a single number used for previous in the next interation
+//   },0)
+//   // console.log('sum', sum);
+//   return sum;
+// };
+
 sum(array);
 
-function productAll (array) {
-  // your code here
-};
 
-function objectify (array) {
-  // your code here
-};
 
-function luckyNumbers (array) {
-  // your code here
-};
+
+
+
+
+
+
+
+
+
+// function productAll (array) {
+//   // your code here
+//   let product = array.reduce(function(previous, current){
+//     // console.log(current);
+//     return previous * current;
+//   })
+//   // console.log(product);
+//   return product;
+// };
+//
+// productAll(array);
+//
+// let array2 = [[ 'Thundercats', '80s' ],
+//              [ 'The Powerpuff Girls', '90s' ],
+//              [ 'Sealab 2021', '00s' ]];
+
+// function objectify (array) {
+//   // your code here
+//   let objects = array.reduce(function(newObj, current){
+//     // console.log(current[0]);
+//     // let key = current[0];
+//     // // console.log(key);
+//     // console.log(newObj);
+//     // return newObj[key];
+//     // return previous[current[0]];
+//
+//   },{})
+//   // console.log(objects);
+// };
+//
+// objectify(array2);
+
+// let array3 = [ 30, 48, 11, 5, 32 ];
+//
+// function luckyNumbers (array) {
+//   // your code here
+//   let luckyNums = array.reduce(function(previous, current, index){
+//     // console.log(previous);
+//     if(index === array.length-1){
+//       // console.log("and " + current);
+//       let lastNumber = "and " + current;
+//       return lastNumber;
+//     }
+//     return previous + ' ' + current + lastNumber;
+//   }, "Your lucky numbers are:")
+// };
+//
+// luckyNumbers(array3);
 
 module.exports = {
-  sum: sum,
-  productAll: productAll,
-  objectify: objectify,
-  luckyNumbers: luckyNumbers
+  sum: sum
+  // productAll: productAll,
+  // objectify: objectify,
+  // luckyNumbers: luckyNumbers
 };
